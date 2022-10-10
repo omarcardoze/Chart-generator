77e77211929ebd9c9453497e24fd40f52ecf91b9
Omar Cardoze <cardozeomar@gmail.com>
2022-10-09 20:13:32 -0500

    fix textareas box center

---
+++ src/components/InputData.tsx
@@ -0,0 +1,43 @@
+import { useCallback } from "react"
+
+const InputData = ({ setData, setLabels }) => {
+
+    const handleData = useCallback((event: any) => {
+        const data = event.target.value
+        setData(data.split(','))
+    }, [])
+
+    const handleLabels = useCallback((event: any) => {
+        const labels = event.target.value
+        setLabels(labels.split(','))
+    }, [])
+
+    return (
+        <section className="flex bg-gray-50 w-3/6 justify-center items-center flex-col">
+            <p className="text-slate-800 text-center mt-5">Import your data values seperated by commas</p>
+            <div>
+                <textarea
+                    name="data"
+                    id="data"
+                    placeholder='Your data here example 122,341,233'
+                    rows={5}
+                    cols={33}
+                    className='m-5 p-5'
+                    onChange={handleData}
+                />
+
+                <textarea
+                    name="labels"
+                    id="labels"
+                    placeholder='Your labels here example A,B,C'
+                    rows={5}
+                    cols={33}
+                    className='m-5 p-5'
+                    onChange={handleLabels}
+                />
+            </div>
+        </section>
+    )
+}
+
+export default InputData
---
+++ src/utils/chartOptions.ts
@@ -0,0 +1,13 @@
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top' as const,
+        },
+        title: {
+            display: true,
+            text: 'My Chart',
+        },
+    },
+};
+
