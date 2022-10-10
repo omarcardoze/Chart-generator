bcc111c5f7d6271063472effaa50870faf6bb0b5
Omar Cardoze <cardozeomar@gmail.com>
2022-10-10 13:48:36 -0500

    add netlify badge to readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/246c060a-395d-4fb8-8f91-308932d7b012/deploy-status)](https://app.netlify.com/sites/chartsgenerator/deploys)
+
 # Chart Generator
 
 ![Chart Generator](./src/assets/chartgenerator-screen.png)
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -34,8 +34,10 @@ export default function Home() {
 
             <aside className='w-80 h-screen border justify-center'>
                 <button onClick={onButtonClick} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center absolute bottom-0 ml-5 mb-10 ">
-                    <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
-                    <span>Exportar</span>
+                    <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+                    </svg>
+                    <span>Export</span>
                 </button>
             </aside>
 
